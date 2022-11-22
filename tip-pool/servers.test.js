d85b3ec6a7b200abc58409a012b79de921e810bc
Matthew Baer <matt.baer77@gmail.com>
2022-11-22 00:32:09 -0500

    Test teardown started

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -2,6 +2,7 @@ describe("Servers test (with setup and tear-down)", function() {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
+
   });
 
   it('should add a new server to allServers on submitServerInfo()', function () {
@@ -9,9 +10,19 @@ describe("Servers test (with setup and tear-down)", function() {
 
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
+
+    //tests go here
+
   });
 
   afterEach(function() {
     // teardown logic
+    // serverNameInput.value ='';
+    console.log("after");
+    // serverId = 0;
+    // ^ MATT, TURN THIS BACK ON
+    serverTbody.innerHTML = '';
+    allServers = {};
+
   });
 });
